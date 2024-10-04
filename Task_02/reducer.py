0edcc6e9934378932c5f08e1c2f45e2b58420550
wolfgerFFF <141361747+wolfgerFFF@users.noreply.github.com>
2024-10-04 20:47:27 +0300

    Update reducer.py

--- Task_02/reducer.py
+++ Task_02/reducer.py
@@ -1,53 +1,22 @@
 import sys
-
 import numpy as np
-
-
-
 # Инициализация переменных
-
 total_count = 0
-
 total_price = 0
-
 total_price_squared = 0
 
-
-
 # Чтение и обработка данных от mapper
-
 for line in sys.stdin:
-
     key, value = line.strip().split('t')
-
-    
-
     if key == "price":
-
-        price = float(value)
-
-        
-
-        # Увеличение счетчика общего количества значений и общей суммы цен
-
+        price = float(value) 
+# Увеличение счетчика общего количества значений и общей суммы цен
         total_count += 1
-
         total_price += price
-
         total_price_squared += price ** 2
-
-
-
 # Вычисление среднего значения и дисперсии
-
 mean_price = total_price / total_count
-
 variance_price = (total_price_squared / total_count) - (mean_price ** 2)
-
-
-
 # Вывод результатов
-
 print("Mean price: {:.2f}".format(mean_price))
-
 print("Variance of price: {:.2f}".format(variance_price))
